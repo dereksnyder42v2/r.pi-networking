58d9e326aebde3502c6013949d815339d76a786a
dereksnyder42v2 <dereksnyder42@gmail.com>
2018-04-27 00:46:58 -0400

    changes, idk

--- wifi_handler.sh
+++ wifi_handler.sh
@@ -7,6 +7,12 @@
 #    3: [not used]
 #    4: incorrect usage (bad cmd args or uid not sudo)
 
+# $direc should be set by config ... wouldn't hurt to double check ;)
 direc='/home/pi/r.pi-networking'
+logfile='/var/log/r.pi-networking/log.txt'
 
 cd $direc
+if [ $UID -ne 0 ]; then
+	echo 'Not root.' >> $logfile
+fi
+
